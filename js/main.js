aabb24a8b0fff3816e0dd35e636f50c97d3b9c4c
Al Amin <hrhridoydigital@gmail.com>
2021-05-24 12:56:05 +0600

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// select header 
+let selectHeader = document.querySelector("#header");
+
+
+// sticky menu
+window.onscroll=()=>{
+    // Add class
+    if(this.scrollY > 100){
+        selectHeader.classList.add("sticky");
+    // Remove class
+    }else{
+        selectHeader.classList.remove("sticky");
+    }
+}
+
+
